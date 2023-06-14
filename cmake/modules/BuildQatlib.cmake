d5b1adc8ffe07cf2561bd673dfe9ce09084f6e61
Liu Miaomiao <miaomiao.liu@intel.com>
2023-06-14 08:53:30 +0000

    change qat and usdm libraries into SHARED

--- cmake/modules/BuildQatlib.cmake
+++ cmake/modules/BuildQatlib.cmake
@@ -9,7 +9,7 @@ function(build_qatlib)
     GIT_SHALLOW TRUE
     GIT_CONFIG advice.detachedHead=false)
 
-  set(qatlib_cflags "-Wno-error -fno-lto")
+  set(qatlib_cflags "-fPIC")
   set(install_cmd ${make_cmd} install)
   set(configure_cmd0 "./autogen.sh")
   if(WITH_QATLIB_inContainer)
@@ -22,9 +22,9 @@ function(build_qatlib)
   ExternalProject_Add(qatlib_ext
       ${source_dir_args}
       CONFIGURE_COMMAND  ${configure_cmd0} COMMAND ${configure_cmd1}
-      BUILD_COMMAND ${make_cmd}
+      BUILD_COMMAND ${make_cmd} CC=${CMAKE_C_COMPILER} EXTRA_CFLAGS=${qatlib_cflags}
       BUILD_IN_SOURCE 1
-      BUILD_BYPRODUCTS "<SOURCE_DIR>/.libs/libqat.a" "<SOURCE_DIR>/.libs/libusdm.a"
+      BUILD_BYPRODUCTS "<SOURCE_DIR>/.libs/libqat.so" "<SOURCE_DIR>/.libs/libusdm.so"
       INSTALL_COMMAND ${install_cmd})
   unset(make_cmd)
 
@@ -38,7 +38,7 @@ function(build_qatlib)
       ${source_dir}/quickassist/utilities/libusdm_drv/include)
   set(Qatlib_LIB ${source_dir}/.libs)
 
-  add_library(Qatlib::qat STATIC IMPORTED GLOBAL)
+  add_library(Qatlib::qat SHARED IMPORTED GLOBAL)
   add_dependencies(Qatlib::qat qatlib_ext)
   file(MAKE_DIRECTORY ${Qatlib_INCLUDE_DIRS})
   set_target_properties(Qatlib::qat PROPERTIES
@@ -46,12 +46,12 @@ function(build_qatlib)
     IMPORTED_LINK_INTERFACE_LANGUAGES "C"
     IMPORTED_LOCATION "${Qatlib_LIB}/libqat.a")
 
-  add_library(Qatlib::usdm STATIC IMPORTED GLOBAL)
+  add_library(Qatlib::usdm SHARED IMPORTED GLOBAL)
   add_dependencies(Qatlib::usdm qatlib_ext)
   set_target_properties(Qatlib::usdm PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES "${Qatlib_INCLUDE_DIRS}"
     IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-    IMPORTED_LOCATION "${Qatlib_LIB}/libusdm.a")
+    IMPORTED_LOCATION "${Qatlib_LIB}/libusdm.so")
 
   unset(source_dir)
 endfunction()
